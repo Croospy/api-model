676461c9838dd58aae74450466f8b9fcf368cd4f
Croospy <benjamin.malfait27@gmail.com>
2024-05-08 16:14:09 +0200

    Update train.py

--- train/train.py
+++ train/train.py
@@ -41,7 +41,7 @@ def train_model(df: pd.DataFrame) -> ClassifierMixin:
 
 
 if __name__ == "__main__":
-    df = ingest_data(r'train\titanic.xls')
+    df = ingest_data('train/titanic.xls')
     df = clean_data(df)
     model = train_model(df)
     joblib.dump(model, "model_titanic.joblib")
